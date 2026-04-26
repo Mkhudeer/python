13889bd8383876596c0b5fc0cd80fcaa4d447c88
Mohamad <mohamadkhudeer@gmail.com>
2026-04-26 13:33:04 +0200

    add Exercises 1

---
+++ Exercises 1/main.py
@@ -0,0 +1,15 @@
+age = 31
+print(age)
+
+name = "mohamad"
+print("Hello,", name)
+
+fruits = ["apple", "banana", "orange", "grape", "mango"]
+print(fruits[1])
+
+number = "123"
+number_int = int(number)
+print(number_int + 10)
+
+import math
+print(math.sqrt(16))
